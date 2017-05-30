c40bab76d53e8fec5fe38ebf99f927fad21c1f71
Hauke Stange <hauke@jimdo.com>
2017-05-30 17:40:42 +0200

    Return list of scores for cast

--- src/ScoreAnalyzer.ts
+++ src/ScoreAnalyzer.ts
@@ -1,11 +1,14 @@
 import { CategoryValidator } from './categories';
+import Score from './Score';
 
 export default class ScoreAnalyzer {
   public constructor(private validators: CategoryValidator[]) { }
 
-  public scores() {
-    this.validators.forEach((validator) => {
-      validator([1, 2, 3, 4, 5]);
-    });
+  public scores(cast: Array<number>): Score[] {
+    return this.validators.map((validator) => {
+      return validator(cast);
+    }).filter((score) => {
+      return score !== null;
+    }) as Score[];
   }
 }
--- src/__tests__/ScoreAnalyzer.test.ts
+++ src/__tests__/ScoreAnalyzer.test.ts
@@ -1,7 +1,9 @@
+import * as category from '../categories';
+import Score from '../Score';
 import ScoreAnalyzer from '../ScoreAnalyzer';
 
 describe('ScoreAnalyzer', () => {
-  describe('scores', () => {
+  describe('constructor', () => {
     it('uses given category validators', () => {
       const validatorOne = jest.fn();
       const validatorTwo = jest.fn();
@@ -11,10 +13,31 @@ describe('ScoreAnalyzer', () => {
         validatorTwo,
       ]);
 
-      analyzer.scores();
+      analyzer.scores([1, 2, 3, 4, 5, 6]);
 
       expect(validatorOne).toHaveBeenCalled();
       expect(validatorTwo).toHaveBeenCalled();
+    });
+  });
+
+  describe('scores', () => {
+    let analyzer: ScoreAnalyzer;
+
+    beforeEach(() => {
+      analyzer = new ScoreAnalyzer([
+        category.aces, category.twos, category.threes, category.fours, category.fives, category.sixes,
+        category.threeOfAKind, category.fourOfAKind, category.fullHouse,
+        category.smallStraight, category.largeStraight,
+        category.chance, category.yahtzee
+      ]);
+    });
+
+    it('returns list of possible scores', () => {
+      const scores = analyzer.scores([1, 1, 1, 2, 2]);
+      expect(scores).toContainEqual(new Score(category.ACES, 3));
+      expect(scores).toContainEqual(new Score(category.TWOS, 4));
+      expect(scores).toContainEqual(new Score(category.THREE_OF_A_KIND, 7));
+      expect(scores).toContainEqual(new Score(category.FULL_HOUSE, 25));
     });
   });
 });
