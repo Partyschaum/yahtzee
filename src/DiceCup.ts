53c7f7e737e83aa1664cee210435c0abd3f7d880
Hauke Stange <partyschaum@posteo.de>
2017-06-17 15:01:46 +0200

    Move dice to method

--- src/DiceCup.ts
+++ src/DiceCup.ts
@@ -1,13 +1,7 @@
 export default class DiceCup {
-  private numberOfDices: number;
-
-  public constructor(dices: number) {
-    this.numberOfDices = dices;
-  }
-
-  public cast(): Array<number> {
+  public cast(numberOfDice: number): Array<number> {
     const pips: Array<number> = [];
-    for (let i = 0; i < this.numberOfDices; i++) {
+    for (let i = 0; i < numberOfDice; i++) {
       pips.push(Math.floor(Math.random() * 6) + 1);
     }
     return pips;
--- src/__tests__/DiceCup.test.ts
+++ src/__tests__/DiceCup.test.ts
@@ -2,20 +2,20 @@ import DiceCup from '../DiceCup';
 
 describe('DiceCup', () => {
   it('casts five numbers between 1 and 6', () => {
-    const numberOfDices = 5;
-    const cup = new DiceCup(numberOfDices);
-    const cast = cup.cast();
+    const numberOfDice = 5;
+    const cup = new DiceCup();
+    const cast = cup.cast(numberOfDice);
 
-    expect(cast).toHaveLength(numberOfDices);
+    expect(cast).toHaveLength(numberOfDice);
     cast.forEach((pip) => {
       expect(pip).toBeGreaterThanOrEqual(1);
       expect(pip).toBeLessThanOrEqual(6);
     });
   });
 
-  it('casts exactly as many dices as are in the cup', () => {
-    const numberOfDices = 10;
-    const cup = new DiceCup(numberOfDices);
-    expect(cup.cast()).toHaveLength(numberOfDices);
+  it('casts exactly as many dice as are in the cup', () => {
+    const numberOfDice = 10;
+    const cup = new DiceCup();
+    expect(cup.cast(numberOfDice)).toHaveLength(numberOfDice);
   });
 });
